dd4e9957e9648d8114cb4eaeba11f1a6b1762909
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2025-12-29 07:36:49 +0700

    chinh sua csharp app

---
+++ Backend - CSharp/myapp/myapp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace myapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Products = new List<int> { 5, 6, 7, 10 };
+            var q = from p in Products where p > 6 select p;
+            foreach (var item in q)
+            {
+                Console.WriteLine(item); 
+            }
+
+        }
+    }
+}
--- Backend - DjangoApp/myapp/webapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from webapp import views
-
-urlpatterns = [
-    path('home', views.home, name='home'),
-    path('home/<int:id>', views.detail, name='detail')
-]
