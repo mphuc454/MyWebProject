deb0471e14a3b41372c845e85822b53c684fad90
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2026-01-01 17:25:01 +0700

    javascript test

--- WebTravelTour/myapp/backend/js/home.js
+++ WebTravelTour/myapp/backend/js/home.js
@@ -1,4 +1,15 @@
-let a = "Day doan van text";
-// let chuoi = a.slice(0,3);
-let chuoi = a.slice(-4,)
-console.log(chuoi)
+
+let arr1 = [5,6,7]
+let arr2 = [true,false,'hello']
+let newArr = arr1.concat(arr2)
+console.log(newArr)
+newArr.push('loop', 'bigguy')
+console.log(newArr)
+newArr.unshift('start', 10)
+console.log(newArr)
+newArr.pop()
+newArr.pop()
+newArr.shift()
+console.log(newArr)
+console.log(newArr.slice(2,5))
+console.log(newArr.includes(4))
